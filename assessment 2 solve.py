e544caec07f132aff22d6e48791f6568ed1cfae0
Hazem <162580798+Hazemqup@users.noreply.github.com>
2024-11-08 14:26:02 +0000

    Add files via upload
    Assessment number 2 done :)

---
+++ assessment 2 solve.py
@@ -0,0 +1,13 @@
+n1 =int(input("Enter your first number"))
+n2 =int(input("Enter your first number"))
+
+sums = n1 + n2
+difference = n1 -  n2
+product = n1 * n2
+quotient = n1 / n2
+
+print(n1,"sums", n2,"is",sums)
+print(n1,"difference", n2,"is",difference)
+print(n1,"product", n2,"is" ,product)
+print(n1,"quotient", n2,"is" ,quotient)
+
