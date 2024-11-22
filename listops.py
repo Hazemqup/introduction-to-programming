c95d5689b6ab2617e2d9170416d2aad44c982743
Hazem <162580798+Hazemqup@users.noreply.github.com>
2024-11-22 13:55:04 +0000

    Add files via upload
    assessment number 6 data structures done

---
+++ listops.py
@@ -0,0 +1,25 @@
+the_num_list=[10,20,32,50,70,100,200,400,550,1000]
+
+def number_list(num):
+    print("The list is:", list(num))
+
+def sum_of_numbers(num):
+    print("The total of all the numbers:", sum(num))
+
+def largest_number(num):
+    print("The most largest number is:", max(num))
+
+def reverse_the_list(num):
+    print("The list in reverse:", list(reversed(num)))
+
+def smallest_number(num):
+    print("The smallest number is:", min(num))
+
+
+
+
+number_list(the_num_list)
+sum_of_numbers(the_num_list)
+largest_number(the_num_list)
+reverse_the_list(the_num_list)
+smallest_number(the_num_list)
